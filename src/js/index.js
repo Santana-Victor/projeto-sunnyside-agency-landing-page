fac0240de576d1b86d4396ffa5f9dcd46cbe2f2f
Santana-Victor <santanavictor592@gmail.com>
2023-06-08 17:20:53 -0300

    6ª commit - alterações no index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,18 @@
 const abrirMenu = document.getElementById("botao-menu-mobile");
+const menu = document.getElementById("nav");
+const hamburger = document.getElementById("hamburger");
 
 function mudancaMenu(event) {
     if (event.type === "touchstart") event.preventDefault();
-    const menu = document.getElementById("nav");
-    menu.classList.toggle("aberto")
+    const menuAberto = menu.classList.contains("aberto");
+    menu.classList.toggle("aberto");
+    if (menuAberto) {
+        hamburger.classList.remove("fa-xmark");
+        hamburger.classList.add("fa-bars");
+    } else {
+        hamburger.classList.remove("fa-bars");
+        hamburger.classList.add("fa-xmark");
+    };
 };
 abrirMenu.addEventListener("click", mudancaMenu);
 abrirMenu.addEventListener("touchstart", mudancaMenu);
